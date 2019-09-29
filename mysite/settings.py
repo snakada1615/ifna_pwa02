9bf6671f3cf8e8a39f1d70553bf98f56b51bf4a4
snakada <khf04440@nifty.com>
2019-09-29 14:59:52 +0900

    Fix: offline cash partially success (^^)

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,9 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'myApp/templates/myApp', 'serviceworker.js')
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'myApp/templates/myApp', 'serviceworker.js')
 # PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'myApp', 'serviceworker.js')
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'myApp/static/js', 'serviceworker.js')
+# PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'myApp/static/js', 'serviceworker.js')
 
 
 # Quick-start development settings - unsuitable for production
