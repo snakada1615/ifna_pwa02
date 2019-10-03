33a7b3997d3a34596916050e5a20defd63b133f7
snakada <khf04440@nifty.com>
2019-10-04 07:29:39 +0900

    Fix: struggling around service worker

--- myApp/templates/myApp/serviceworker.js
+++ myApp/templates/myApp/serviceworker.js
@@ -59,11 +59,14 @@ self.addEventListener('activate', event => {
   );
 });
 
+
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
+  const url = new URL(event.request.url);
+  console.log(url.pathname);
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
