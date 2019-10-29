b51a5f9f911b694c286108a5829031f86149cae3
snakada <khf04440@nifty.com>
2019-10-29 03:31:40 +0200

    Fix: to show selected nperson list

--- myApp/views.py
+++ myApp/views.py
@@ -179,7 +179,7 @@ class Person_ListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset().filter(familyid = self.kwargs['familyid']).order_by('age')
-#        return queryset
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
