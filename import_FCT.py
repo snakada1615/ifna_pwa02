eddba57c8115073ddcc1d6b50a1bdfc7b96c9824
snakada <khf04440@nifty.com>
2020-03-06 17:53:03 +0900

    fix: import_ftc

--- import_FCT.py
+++ import_FCT.py
@@ -17,7 +17,7 @@ with open('fct.csv', 'r') as f:
     b = csv.reader(f)
     print(b)
     header = next(b)
-    sql = 'insert into myApp_FCT (FCT_id, food_grp_id, food_item_id,Food_grp,Food_name,Crop_ref,Edible,Energy,WATER,Protein,Fat,Carbohydrate,Fiber,ASH,CA,FE,MG,P,K,NA,ZN,CU,VITA_RAE,RETOL,B_Cart_eq,VITD,VITE,THIA,RIBF,NIA,VITB6C,FOL,VITB12,VITC) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
+    sql = 'insert into myApp_FCT (FCT_id, food_grp_id, food_item_id,Food_grp,Food_name,Crop_ref,Edible,Energy,WATER,Protein,Fat,Carbohydrate,Fiber,ASH,CA,FE,MG,P,K,NA,ZN,CU,VITA_RAE,RETOL,B_Cart_eq,VITD,VITE,THIA,RIBF,NIA,VITB6C,FOL,VITB12,VITC,portion_size) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,100)'
     for t in b:
         data = t
 #        print(t)
