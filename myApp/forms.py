f04ad8005316539210bed5497a9c84b77ca61ba6
shunichi <khf04440@nifty.com>
2020-04-20 18:10:56 +0900

    Crop_Editorの設計３：Datatablesからの品目入力

--- myApp/forms.py
+++ myApp/forms.py
@@ -68,15 +68,17 @@ class ProfileForm(forms.ModelForm):
 
 class Crop_Feas_Form(forms.ModelForm):
   crop_name = forms.CharField(max_length=200)
+  crop_name_id = forms.IntegerField()
 
   class Meta:
     model = Crop_Feasibility
-    fields = ('crop_name', 'feas_DRI_e', 'feas_DRI_e', 'feas_DRI_a', 'feas_DRI_f', 'feas_soc_acceptable', 'feas_soc_acceptable_wo',
+    fields = ('crop_name', 'crop_name_id', 'feas_DRI_e', 'feas_DRI_e', 'feas_DRI_a', 'feas_DRI_f', 'feas_soc_acceptable', 'feas_soc_acceptable_wo',
               'feas_soc_acceptable_c5', 'feas_prod_skill', 'feas_workload', 'feas_tech_service', 'feas_invest_fixed',
               'feas_invest_variable', 'feas_availability_prod','feas_availability_non', 'feas_affordability',
               'feas_storability', 'crop_score')
     widgets = {
       'crop_score': forms.HiddenInput(),
       'crop_name': forms.TextInput(attrs={'readonly': 'readonly'}),
+      'crop_name_id': forms.HiddenInput(),
     }
 
