0db8e6c6dac63120b03047204803aaeead7567f6
shunichi <khf04440@nifty.com>
2020-04-20 18:23:43 +0900

    Crop_Editorの設計4：保存時のsuccessful URLを追加

--- myApp/views.py
+++ myApp/views.py
@@ -1210,6 +1210,7 @@ class Crop_Feas_CreateView(LoginRequiredMixin, CreateView):
   model = Crop_Feasibility
   form_class = Crop_Feas_Form
   template_name = 'myApp/Crop_Feas_form.html'
+  success_url = reverse_lazy('index02')
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
