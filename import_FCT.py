246a9e84fa6837da5c42df52250ba9b70a1aee7a
shunichi <khf04440@nifty.com>
2020-10-01 21:34:45 +0900

    fctの修正

--- import_FCT.py
+++ import_FCT.py
@@ -17,7 +17,7 @@ with open('fct2.csv', 'r') as f:
     b = csv.reader(f)
     print(b)
     header = next(b)
-    sql = 'insert into myApp_FCT (FCT_id, food_grp_id, food_item_id,Food_grp,Food_name,Crop_ref,Edible,Energy,WATER,Protein,Fat,Carbohydrate,Fiber,ASH,CA,FE,MG,P,K,NA,ZN,CU,VITA_RAE,RETOL,B_Cart_eq,VITD,VITE,THIA,RIBF,NIA,VITB6C,FOL,VITB12,VITC,portion_size_init, Food_grp_unicef) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,30,?)'
+    sql = 'insert into myApp_FCT (FCT_id, food_grp_id, food_item_id,Food_grp,Food_name,Crop_ref,Edible,Energy,WATER,Protein,Fat,Carbohydrate,Fiber,ASH,CA,FE,MG,P,K,NA,ZN,CU,VITA_RAE,RETOL,B_Cart_eq,VITD,VITE,THIA,RIBF,NIA,VITB6C,FOL,VITB12,VITC,portion_size_init, Food_grp_unicef) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,30,?,?)'
     for t in b:
         data = t
         cursor.execute(sql, data)
