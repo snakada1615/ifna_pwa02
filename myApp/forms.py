7d48395c06bef02f9483f8a4106bb4144f4a9c11
tult_tt <tult_tt@rikkeisoft.com>
2020-10-19 15:58:28 +0700

    update regex expression

--- myApp/forms.py
+++ myApp/forms.py
@@ -154,9 +154,9 @@ class UserCreateForm(UserCreationForm):
   def clean(self):
     cleaned_data = super(UserCreateForm, self).clean()
     if 'username' in cleaned_data and cleaned_data['username'] != '':
-      pattern = "[a-zA-Z0-9\-._@+]"
+      pattern = "[^a-zA-Z0-9\-._@+]"
       username = self.cleaned_data['username']
-      if not re.match(pattern, username):
+      if re.search(pattern, username):
         raise ValidationError({'username': [_('Your input for [%(field_name)s] is invalid! Please confirm and try again.') % {'field_name': _('Username')}]})
     self.cleaned_data['is_staff'] = 1  # staffステータスの設定
     return cleaned_data
